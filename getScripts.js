1a452b34df800b25f2e4e3421b1554147795249b
Robert Morel <robertchristophermorel@gmail.com>
2019-11-16 12:14:54 +0000

    added some error checking

--- getScripts.js
+++ getScripts.js
@@ -3,28 +3,42 @@ const puppeteer = require('puppeteer');
 //Execute script using node getScripts.js {site url} {filetype}
 //EG: node getScripts.js https://imgur.com css
 
-const url = process.argv[2];
-const filetype = process.argv[3];
-console.log(`Searching: ${url}`);
+function main(){
+  const url = process.argv[2];
+  const filetype = process.argv[3];
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  if (url == null ){
+    console.log("Please enter a url as the first parameter. e.g. https://imgur.com");
+    return;
+  }
 
-  // Emitted when the page produces a request
-  console.group(`The following ${filetype} files were requested:`);
-  page.on('request', request => {
-      let requestUrl = request.url();
-      if( requestUrl.includes( filetype )){
-         console.info(`Request: ${request.url()}`);
-      }
-  });
-  console.groupEnd();
+  if (filetype == null ){
+    console.log("Please enter a filetype as the 2nd parameter. e.g. css or js");
+    return;
+  }
+  console.log(`Searching: ${url}`);
 
-  // Emitted after the page is closed
-  page.once('close', () => console.info('Page is closed'));
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  await page.goto(url);
-  
-  await browser.close();
-})();
+    // Emitted when the page produces a request
+    console.group(`The following ${filetype} files were requested:`);
+    page.on('request', request => {
+        let requestUrl = request.url();
+        if( requestUrl.includes( filetype ) && !requestUrl.includes("base64")){
+          console.info(`Request: ${request.url()}`);
+        }
+    });
+    console.groupEnd();
+
+    // Emitted after the page is closed
+    page.once('close', () => console.info('Page is closed'));
+
+    await page.goto(url);
+    
+    await browser.close();
+  })();
+}
+
+main();
